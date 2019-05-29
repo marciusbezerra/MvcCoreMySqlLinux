0c541ef64a3b32a119be579b3d1cf4795c3deb64
Marcius Bezerra <marciusbezerra@outlook.com>
2019-05-28 21:11:22 -0700

    Commit inicial

---
+++ Models/Livro.cs
@@ -0,0 +1,10 @@
+namespace MvcCoreMySqlLinux.Models
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Autor { get; set; }
+        public int PublicadoEm { get; set; }
+    }
+}
